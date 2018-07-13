3174dcbd5a396781701ffa87c6a5751f88452af8
BEAM-User <neemaahm@gmail.com>
2018-07-13 12:44:31 -0400

    Added resolution feature and fixed some variables names

--- Main.py
+++ Main.py
@@ -13,36 +13,49 @@ def inputWaveform():
 
 #Gets valid input for frequency
 def inputFrequency():
-    isFloat = False
-    while isFloat == False:
+    isValid = False
+    while isValid == False:
         frequency = input("Frequency(Hz): ")
         try:
             frequency = abs(float(frequency))
-            isFloat = True
+            isValid = True
         except ValueError:
             print("\nPlease enter a valid option.")
     return frequency
 
-#Sets the waveform and frequency variables
+#Gets valid input for resolution
+def inputRes():
+    isValid = False
+    while isValid == False:
+        res = input("Waveform Resolution(10-1000): ")
+        try:
+            res = abs(int(res))
+            if res >= 10 and res <= 1000:
+                isValid = True
+            else:
+                print("\nPlease enter a number within the range.")
+        except ValueError:
+            print("\nPlease enter a valid input.")
+    return res
+
+#Sets the waveform, frequency, and resolution variables
 waveform = inputWaveform()
 frequency = inputFrequency()
+res = inputRes()
 
 #Shows the user what they have chosen as their waveform and its period
 print(str(frequency) + "Hz " + waveform + " wave")
 period = 1/frequency
 print("Period: " + str(period) + " sec\n")
-#Generates variables
 
-#Generates dictionaries used in the rest of the code
+#Creating dictionaries used in the rest of the code
 rawValues = {0:0}
 outputValues = {0:0}
 
 #Fills the rawValues dictionary with sin wave values
 def generateSin():
-    #for x in range(1,int(6283*period)):
-    for x in range(1,int(62*period)):
-        #value = math.sin(x*frequency*0.001)
-        value = math.sin(x * frequency * 0.1)
+    for x in range(1,int(math.pi*2*res*period)):
+        value = math.sin(x * frequency * (1.0/res))
         rawValues.update({x:value})
         if value<0.001:
             if value>-0.001:
@@ -57,19 +70,18 @@ def generateSqr():
 #Processes the rawValues dictionary and fills the outputValues dictionary with final output values
 def processValues():
     for x in rawValues:
-        #if x % 10 == 0:
         if x%1 == 0:
             val = int((rawValues[x]+1)*2048)
             outputValues.update({x:val})
 
 #Outputs values to the 12 bit DAC (prints values to console just for now)
 def output():
-    while True:
-        for x in outputValues:
-                print(str(x) + ": " + str(outputValues[x]))
-                if outputValues[x] == 4096:
-                    break
-                time.sleep(0.01)
+    #while True:
+    for x in outputValues:
+            print(str(x) + ": " + str(outputValues[x]))
+            if outputValues[x] == 4096:
+                break
+            time.sleep(0.01)
 
 if waveform == "sin":
     generateSin()
